f3ee58c8cb4f6078abf02e59509575a2740df3f8
George Philbrick <gphilbri@gmail.com>
2019-12-20 23:37:59 -0500

    Handle Client Responses

--- lib/WebSocketConn/WebSocketConn.h
+++
@@ -1,7 +0,0 @@
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <WebSocketsServer.h>
-
-void WifiConnect(const String &ssid, const String &password);
-void StartWebSocket(WebSocketsServer &webSocket);
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length);
--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,36 @@
 #include <Arduino.h>
-#include <WebSocketConn.h>
+#include <ESP8266WiFi.h>
+#include <WebSocketsServer.h>
 #include <CodeChecker.h>
 #include <Keypad.h>
 
 //Websocket and Wifi Constants and Variables
-const char * SSID = "SIMPLEWIFI";
-const char * PWD = "198Geniuses";
+const char *SSID = "SIMPLEWIFI";
+const char *PWD = "198Geniuses";
 
 WebSocketsServer webSocket(81);
 
 //Code Constants and Variables
-const byte NUM_CODES = 5;
+const byte NUM_CODES = 2;
 const byte CODE_LENGTH = 5;
 
 CodeChecker codes = CodeChecker(NUM_CODES, CODE_LENGTH);
-bool codesSent = false;
 
 const byte STANDBY_CODE = 0;
-const byte RELOAD_CODE = 1;
-const byte LASER_CODE = 2;
-const byte CALIBRATION_CODE = 3;
-const byte CONSENT_CODE = 4;
+const byte CONSENT_CODE = 1;
 
 //Keypad Constants and Variables
 const byte ROWS = 4;
 const byte COLS = 3;
 
 char hexaKeys[ROWS][COLS] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'*','0','#'}
-};
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'},
+    {'*', '0', '#'}};
 
 byte rowPins[ROWS] = {D0, D1, D2, D3}; //connect to the row pinouts of the kpd
-byte colPins[COLS] = {D5, D6, D7}; //connect to the column pinouts of the kpd
+byte colPins[COLS] = {D5, D6, D7};     //connect to the column pinouts of the kpd
 
 Keypad kpd = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
@@ -42,12 +38,23 @@ unsigned long lastMillis;
 
 // gun states
 bool standby = false;
+bool laser = false;
+bool reloadLock = true;
+bool calibrated = false;
+bool consent = false;
+
+//Laser
+const byte LASER_PIN = D8;
 
 // function prototypes
 void SendCodes();
 bool CheckStandby();
+void WifiConnect(const String &ssid, const String &password);
+void StartWebSocket(WebSocketsServer &webSocket);
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length);
 
-void setup() {
+void setup()
+{
   //Communication Setup
   Serial.begin(9600);
   WifiConnect(SSID, PWD);
@@ -61,15 +68,15 @@ void setup() {
   }
   //Get initial millis
   lastMillis = millis();
+  //Laser and Motor Setup
+  pinMode(LASER_PIN, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   webSocket.loop();
-  if (!codesSent)
-  {
-    SendCodes();
-  }
-  else
+  digitalWrite(LASER_PIN, laser);
+  if (webSocket.connectedClients() > 0)
   {
     if (CheckStandby())
     {
@@ -82,11 +89,9 @@ void SendCodes()
 {
   for (int i = 0; i < webSocket.connectedClients(); i++)
   {
-    String msg = "CODES," + codes.getCode(STANDBY_CODE) + "," + codes.getCode(RELOAD_CODE) + "," + codes.getCode(LASER_CODE) + "," 
-    + codes.getCode(CALIBRATION_CODE) + "," + codes.getCode(CONSENT_CODE);
+    String msg = "CODES," + codes.getCode(STANDBY_CODE) + "," + codes.getCode(CONSENT_CODE);
     Serial.println(msg);
     webSocket.sendTXT(i, msg);
-    codesSent = true;
   }
 }
 
@@ -110,10 +115,74 @@ bool CheckStandby()
         {
           webSocket.sendTXT(i, "STNDBY");
         }
-        
+
         return true;
       }
     }
     return false;
+  }
+}
+
+void WifiConnect(const String &ssid, const String &password)
+{
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  Serial.println("...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println(++i);
+    Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+}
+
+void StartWebSocket(WebSocketsServer &webSocketConn)
+{
+  webSocketConn.begin();
+  webSocketConn.onEvent(webSocketEvent);
+  Serial.println("WebSocket server started.");
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    Serial.printf("[%u] Disconnected!\n", num);
+    standby = false;
+    break;
+  case WStype_CONNECTED:
+    Serial.println("Connection Succeful");
+    SendCodes();
+    break;
+  case WStype_TEXT:
+    Serial.printf("[%u] get Text: %s\n", num, payload);
+    String webSocketMessage = (char *) payload;
+    if (webSocketMessage == "LASERSIGHTON")
+    {
+      Serial.println("LASER TIME BRUHHH");
+      laser = true;
+    }
+    else if (webSocketMessage == "CALIBRATE")
+    {
+      calibrated = true;
+    }
+    else if (webSocketMessage == "MAGLOCKOFF")
+    {
+      reloadLock = false;
+    }
+    else if (webSocketMessage == "MAGLOCKON")
+    {
+      reloadLock = true;
+    }
+    break;
   }
 }
