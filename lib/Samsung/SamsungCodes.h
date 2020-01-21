5f31abb2ff95bbe6e53d1fb46a6284003d561d90
George Philbrick <gphilbri@gmail.com>
2020-01-20 22:17:26 -0500

    Updated Remote Code to New Remote

--- lib/Samsung/SamsungCodes.h
+++ lib/Samsung/SamsungCodes.h
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 
-const uint64_t HEX_POWER = 0xE0E040BF;
-const uint64_t HEX_1 = 0xE0E020DF;
-const uint64_t HEX_2 = 0xE0E0A05F;
-const uint64_t HEX_3 = 0xE0E0609F;
-const uint64_t HEX_4 = 0xE0E010EF;
-const uint64_t HEX_5 = 0xE0E0906F;
-const uint64_t HEX_6 = 0xE0E050AF;
-const uint64_t HEX_7 = 0xE0E030CF;
-const uint64_t HEX_8 = 0xE0E0B04F;
-const uint64_t HEX_9 = 0xE0E0708F;
-const uint64_t HEX_0 = 0xE0E08877;
+const uint64_t HEX_POWER = 0x20DF10EF;
+const uint64_t HEX_1 = 0x20DF8877;
+const uint64_t HEX_2 = 0x20DF48B7;
+const uint64_t HEX_3 = 0x20DFC837;
+const uint64_t HEX_4 = 0x20DF28D7;
+const uint64_t HEX_5 = 0x20DFA857;
+const uint64_t HEX_6 = 0x20DF6897;
+const uint64_t HEX_7 = 0x20DFE817;
+const uint64_t HEX_8 = 0x20DF18E7;
+const uint64_t HEX_9 = 0x20DF9867;
+const uint64_t HEX_0 = 0x20DF08F7;
--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,13 @@
 #include <IRrecv.h>
 #include <IRutils.h>
 #include <SamsungCodes.h>
+#include <Servo.h>
 
 //Websocket and Wifi Constants and Variables
-const char *SSID = "SIMPLEWIFI";
-const char *PWD = "198Geniuses";
+const char *SSID = "THE_WORK_SITE";
+const char *PWD = "godilovemondays";
 
 WebSocketsServer webSocket(81);
-
-const int boi = D10;
 
 //Code Constants and Variables
 const byte NUM_CODES = 2;
@@ -41,7 +40,7 @@ byte colPins[COLS] = {D5, D6, D7};     //connect to the column pinouts of the kp
 Keypad kpd = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 //IR Remote Setup and Constants
-const byte IR_PIN = D4;
+const byte IR_PIN = D9; //IR on RX pin
 
 IRrecv irrecv(IR_PIN);
 
@@ -52,15 +51,19 @@ unsigned long lastMillis;
 // gun states
 bool standby = false;
 bool laser = false;
-bool reloadLock = true;
 bool calibrated = false;
 bool consent = false;
 
 //Laser
 const byte LASER_PIN = D8;
 
-//Motor Setup
+//Motor Setup (These are using TX and RX, so Serial must be disabled)
 const byte MOTOR_PIN = D10; //motor on TX pin
+const byte SERVO_PIN = D4; 
+Servo magServo;
+const int UNLOCKED_POSITION = 0;
+const int LOCKED_POSITION = 80;
+int magServoPosition = LOCKED_POSITION;
 
 // function prototypes
 void SendCodes();
@@ -74,21 +77,22 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 void setup()
 {
   //Communication Setup
-  Serial.begin(9600);
+  //Serial.begin(9600);
   WifiConnect(SSID, PWD);
   StartWebSocket(webSocket);
   // Code Generation
   codes.Init();
-  Serial.println("\nCODES:\n");
+  //Serial.println("\nCODES:\n");
   for (int i = 0; i < NUM_CODES; i++)
   {
-    Serial.println(codes.getCode(i));
+    //Serial.println(codes.getCode(i));
   }
   //Get initial millis
   lastMillis = millis();
   //Laser and Motor Setup
   pinMode(LASER_PIN, OUTPUT);
   pinMode(MOTOR_PIN, OUTPUT);
+  magServo.attach(SERVO_PIN);
   //IR Remote Setup
   irrecv.enableIRIn();
 }
@@ -98,6 +102,7 @@ void loop()
   webSocket.loop();
   digitalWrite(LASER_PIN, laser);
   digitalWrite(MOTOR_PIN, consent);
+  magServo.write(magServoPosition);
   if (webSocket.connectedClients() > 0)
   {
     if (CheckStandby())
@@ -115,7 +120,7 @@ void SendCodes()
   for (int i = 0; i < webSocket.connectedClients(); i++)
   {
     String msg = "CODES," + codes.getCode(STANDBY_CODE) + "," + codes.getCode(CONSENT_CODE);
-    Serial.println(msg);
+    //Serial.println(msg);
     webSocket.sendTXT(i, msg);
   }
 }
@@ -131,11 +136,11 @@ bool CheckStandby()
     char readKey = kpd.getKey();
     if (readKey != NO_KEY)
     {
-      Serial.println(readKey);
+      //Serial.println(readKey);
       if (codes.readCode(readKey, STANDBY_CODE))
       {
         standby = true;
-        Serial.println("Standby");
+        //Serial.println("Standby");
         for (int i = 0; i < webSocket.connectedClients(); i++)
         {
           webSocket.sendTXT(i, "STNDBY");
@@ -158,11 +163,11 @@ bool CheckConsent()
     char readKey = ReadIR();
     if (readKey != NO_KEY)
     {
-      Serial.println(readKey);
+      //Serial.println(readKey);
       if (codes.readCode(readKey, CONSENT_CODE))
       {
         consent = true;
-        Serial.print("Consent");
+        //Serial.print("Consent");
         for (int i = 0; i < webSocket.connectedClients(); i++)
         {
           webSocket.sendTXT(i, "CONSENT");
@@ -183,8 +188,8 @@ char ReadIR()
       irrecv.resume();
       return NO_KEY;
     }
-    serialPrintUint64(results.value, HEX);
-    Serial.print(" ");
+    //serialPrintUint64(results.value, HEX);
+    //Serial.print(" ");
     lastMillis = millis();
     switch (results.value)
     {
@@ -247,29 +252,29 @@ char ReadIR()
 void WifiConnect(const String &ssid, const String &password)
 {
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
-  Serial.println("...");
+  //Serial.print("Connecting to ");
+  //Serial.print(ssid);
+  //Serial.println("...");
 
   int i = 0;
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(1000);
-    Serial.println(++i);
-    Serial.print(' ');
+    //Serial.println(++i);
+    //Serial.print(' ');
   }
 
-  Serial.println('\n');
-  Serial.println("Connection established!");
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());
+  //Serial.println('\n');
+  //Serial.println("Connection established!");
+  //Serial.print("IP address:\t");
+  //dSerial.println(WiFi.localIP());
 }
 
 void StartWebSocket(WebSocketsServer &webSocketConn)
 {
   webSocketConn.begin();
   webSocketConn.onEvent(webSocketEvent);
-  Serial.println("WebSocket server started.");
+  //Serial.println("WebSocket server started.");
 }
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
@@ -277,21 +282,21 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
   switch (type)
   {
   case WStype_DISCONNECTED:
-    Serial.printf("[%u] Disconnected!\n", num);
+    //Serial.printf("[%u] Disconnected!\n", num);
     standby = false;
     laser = false;
     consent = false;
     break;
   case WStype_CONNECTED:
-    Serial.println("Connection Succeful");
+    //Serial.println("Connection Succeful");
     SendCodes();
     break;
   case WStype_TEXT:
-    Serial.printf("[%u] get Text: %s\n", num, payload);
+    //Serial.printf("[%u] get Text: %s\n", num, payload);
     String webSocketMessage = (char *) payload;
     if (webSocketMessage == "LASERSIGHTON")
     {
-      Serial.println("LASER TIME BRUHHH");
+      //Serial.println("LASER TIME BRUHHH");
       laser = true;
     }
     else if (webSocketMessage == "CALIBRATE")
@@ -300,11 +305,11 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
     }
     else if (webSocketMessage == "MAGLOCKOFF")
     {
-      reloadLock = false;
+      magServoPosition = UNLOCKED_POSITION;
     }
     else if (webSocketMessage == "MAGLOCKON")
     {
-      reloadLock = true;
+      magServoPosition = LOCKED_POSITION;
     }
     break;
   }
